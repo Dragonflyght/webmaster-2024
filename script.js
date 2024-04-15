c95ec2bf2ff22f7ab545cb996057f24cdf6e3175
Kayleigh Gustafson <kayleighfayegustafson@gmail.com>
2024-04-15 14:14:14 -0600

    added button animation

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById("body").onscroll = function myFunction() {
     var scrolltotop = document.scrollingElement.scrollTop;
     var target = document.getElementById("home-header");
     var xvalue = "center";
-    var factor = 25;
+    var factor = 40;
     console.log(scrolltotop);
     console.log(window.innerHeight);
     var yvalue = ((scrolltotop / window.innerHeight) * factor) + 50;
