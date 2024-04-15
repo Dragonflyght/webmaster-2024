3de46654b277b4d2ff1b5dd95e5a7e5946775fd1
Dragonflyght <kayleighfayegustafson@gmail.com>
2024-04-15 06:54:57 -0600

    Added parallax

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("body").onscroll = function myFunction() {
+    console.log("Test")
+    var scrolltotop = document.scrollingElement.scrollTop;
+    var target = document.getElementById("home-header");
+    var xvalue = "center";
+    var factor = 25;
+    console.log(scrolltotop);
+    console.log(window.innerHeight);
+    var yvalue = ((scrolltotop / window.innerHeight) * factor) + 50;
+    target.style.backgroundPosition = xvalue + " " + yvalue + "%";
+  }
