a078de3db7826a980f521524151fd1f7d966ef29
Dragonflyght <kayleighfayegustafson@gmail.com>
2024-05-02 20:26:30 -0600

    Added lots of stuff and fixed learn page (kind of)

--- learn.js
+++ learn.js
@@ -1,10 +1,25 @@
 lastSection = "";
 
 document.getElementById("body").onscroll = function scroll() {
+  elements = {
+    "learn-header": [50, -30]
+  }
+  var scrolltotop = document.scrollingElement.scrollTop;
+  for (element in elements) {
+    var target = document.getElementById(element);
+    var xvalue = elements[element][0];
+    var yoffset = target.offsetTop;
+    var factor = elements[element][1];
+    console.log(xvalue, yoffset, factor);
+    var yvalue = ((((scrolltotop-yoffset) / window.innerHeight) * factor) + 50);
+    console.log(yvalue);
+    target.style.backgroundPosition = xvalue + "% " + yvalue + "%";
+  }
+
   elements = ["about","solar","wind","geo", "hydro", "marine", "bio", "cost", "env"]
   var scrolltotop = document.scrollingElement.scrollTop;
   var currentElement = elements[0];
-  var switchPercent = 0.2;
+  var switchPercent = 0.5;
   for (index in elements) {
     document.getElementById("link-section-"+elements[index]).classList.remove("active-page-location");
     document.getElementById("section-"+elements[index]).classList.remove("learn-section-focus");
@@ -14,7 +29,7 @@ document.getElementById("body").onscroll = function scroll() {
     var target = document.getElementById("section-"+element);
     var yvalue = target.offsetTop;
     var distance = scrolltotop - yvalue;
-    if (distance - (window.innerHeight * switchPercent) < 0){
+    if (distance < (target.offsetHeight - (window.innerHeight * switchPercent))){
       currentElement = element;
       break;
     }
@@ -24,17 +39,20 @@ document.getElementById("body").onscroll = function scroll() {
   document.getElementById("link-section-"+currentElement).classList.add("active-page-location");
   document.getElementById("section-"+currentElement).classList.add("learn-section-focus");
   // document.getElementById("learn-background-image").style.backgroundImage = "url('assets/learn-bg-images/"+currentElement+".jpg)"
-  if (currentElement != lastSection) {
-    if (document.getElementById("learn-background-image-2").classList.contains("learn-background-image-inactive")) {
-      document.getElementById("learn-background-image-2").style.backgroundImage = "url('assets/learn-bg-images/"+currentElement+".jpg')";
-      document.getElementById("learn-background-image-2").classList.remove("learn-background-image-inactive");
-    } else {
-      document.getElementById("learn-background-image-1").style.backgroundImage = "url('assets/learn-bg-images/"+currentElement+".jpg')";
-      document.getElementById("learn-background-image-2").classList.add("learn-background-image-inactive");
-    }
-  }
+  // if (currentElement != lastSection) {
+  //   if (document.getElementById("learn-background-image-2").classList.contains("learn-background-image-inactive")) {
+  //     document.getElementById("learn-background-image-2").style.backgroundImage = "url('assets/learn-bg-images/"+currentElement+".jpg')";
+  //     document.getElementById("learn-background-image-2").classList.remove("learn-background-image-inactive");
+  //   } else {
+  //     document.getElementById("learn-background-image-1").style.backgroundImage = "url('assets/learn-bg-images/"+currentElement+".jpg')";
+  //     document.getElementById("learn-background-image-2").classList.add("learn-background-image-inactive");
+  //   }
+  // }
   lastSection = currentElement;
   // document.getElementById("learn-background-image").style.backgroundImage = "url('assets/learn-bg-images/"+currentElement+".jpg')";
   // console.log(document.getElementById('learn-background-image').style.backgroundImage);
 }
 
+function jumpTo() {
+  console.log("hi")
+}
---
+++ references.js
@@ -0,0 +1,20 @@
+document.getElementById("body").onscroll = function scroll() {
+  elements = {
+    "references-header": [50, -40],
+  }
+  var scrolltotop = document.scrollingElement.scrollTop;
+  for (element in elements) {
+    var target = document.getElementById(element);
+    var xvalue = elements[element][0];
+    var yoffset = target.offsetTop;
+    var factor = elements[element][1];
+    console.log(xvalue, yoffset, factor);
+    var yvalue = ((((scrolltotop-yoffset) / window.innerHeight) * factor) + 70);
+    console.log(yvalue);
+    target.style.backgroundPosition = xvalue + "% " + yvalue + "%";
+  }
+
+
+  
+}
+
